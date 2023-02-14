8789daa65c3bf2e5003c20829862fe0637434189
b.polyanok <b.polyanok@2gis.ru>
2023-02-14 21:29:44 +0300

    fix url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='compatibility_schema_42',
     description='Compatibility schema for migration from 0.6.9 to 1+ versions',
     version='1.0.0',
-    url='https://github.com/nikitanovosibirsk/district42',
+    url='https://github.com/miner34006/compatibility_schema',
     author='Polianok Bogdan',
     author_email='bogdan.polianok@gmail.com',
     python_requires='>=3.6',
